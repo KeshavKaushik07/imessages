b1fd4fddd441d891ee7d9a447851f63c6437c5d3
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-06-15 23:21:27 +0530

    add Dockerfile and .dockerignore; implement server static file serving

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import connectDB from "./lib/db.js";
 import colors from 'colors';
 import { clerkMiddleware } from '@clerk/express';
 import cors from 'cors';
+import fs from 'fs';
+import path from "path";
 // package.json
 // "scripts": {
 //   "start": "node -r dotenv/config index.js"
@@ -11,10 +13,16 @@ import cors from 'cors';
 
 const app = express();
 
+const PORT = process.env.PORT || 4000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(),"public");
+
+
 app.use(express.json());
 app.use(clerkMiddleware());
 
-const FRONTEND_URL = process.env.FRONTEND_URL;
+
 app.use(cors({
     origin: FRONTEND_URL,
     credentials: true,
@@ -22,9 +30,17 @@ app.use(cors({
 
 app.get('/health', (req, resp) => {
     resp.status(200).json({ ok: true });
-})
+});
 
-const PORT = process.env.PORT || 4000;
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir));
+
+    app.get("/{*}any",(req,resp,next)=>{
+        resp.sendFile(path.join(publicDir,"index.html"),(err)=> next(err));
+    })
+}
+
+
 app.listen(PORT, () => {
     connectDB();
     console.log(`server is running on PORT ${PORT}`.bgGreen);
