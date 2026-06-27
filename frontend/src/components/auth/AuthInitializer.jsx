4f5813236e607765c74ee8e6879e5ee4b0c374ad
Keshav Kaushik <keshavkaushik955@gmail.com>
2026-06-27 15:31:27 +0530

    correct the path

--- frontend/src/components/auth/AuthInitializer.jsx
+++ frontend/src/components/auth/AuthInitializer.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useAuth } from "@clerk/react";
-import { setTokenGetter } from "../lib/axios";
+import { setTokenGetter } from "../../lib/axios";
 
 export default function AuthInitializer() {
   const { getToken } = useAuth();
